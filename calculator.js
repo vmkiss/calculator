de260c19c050de4ae15858f58044b08cff9ec5f4
vmkiss <veronika.kiss17@gmail.com>
2024-06-25 16:30:29 -0700

    create calculator rows

--- calculator.js
+++ calculator.js
@@ -1,12 +1,3 @@
-/* 1. Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console.
-add
-subtract
-multiply
-divide
-2. A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
-3. Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-*/
-
 let num1;
 let num2;
 let operator;
@@ -45,5 +36,3 @@ function operate(num1, num2, operator) {
         return divide(num1, num2);
     }
 }
-
-console.log(operate(5, 6, "multiply"));
